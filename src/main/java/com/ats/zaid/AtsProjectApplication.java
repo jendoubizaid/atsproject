1206324d0e4283b4bfbee02c9dbe6a8da182c6b0
zaid jendoubi <jendoubizaid@gmail.com>
2017-11-08 10:10:34 +0100

    remove msgs

--- src/main/java/com/ats/zaid/AtsProjectApplication.java
+++ src/main/java/com/ats/zaid/AtsProjectApplication.java
@@ -15,27 +15,6 @@ public class AtsProjectApplication{
 	public static void main(String[] args) {
 
 		ApplicationContext ctx=SpringApplication.run(AtsProjectApplication.class, args);
-//		ProductRepository productRepository=ctx.getBean(ProductRepository.class);
-//		for(Product p:productRepository.findByCategory("Tools")){
-//			System.out.println(p.getProductName());
-//			Iterator it=p.getReviews().iterator();
-//			while (it.hasNext()){
-//				Review r=(Review)it.next();
-//				System.out.println(r.getRating());
-//			}
-//		}
-//		System.out.println("pagination");
-//		int page=1;
-//		int size=20;
-//		for (Product p:productRepository.findAll(new PageRequest(page,size))) {
-//			System.out.println(p.getProductName());
-//		}
-//		ArrayList<String> sortBy=new ArrayList<String>(Arrays.asList("productName","basePrice","category","brand","productMaterial","delivery","details"));
-//		int randomItem=new Random().nextInt(sortBy.size());
-//		System.out.println(">>>>>>>>>>>>>>>>sort");
-//		for (Product p:productRepository.findAll(new Sort(Sort.Direction.ASC,sortBy.get(randomItem))).subList(0,20)) {
-//			System.out.println(p.getBasePrice());
-//		}
 	}
 
 }
